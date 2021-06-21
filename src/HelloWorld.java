e2d8fc14b65744892b0dd2409cd34b43420353b3
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-21 18:58:50 -0500

    exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,59 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello, World!");
-    }
+       int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
 
+//        String myString = "Jesse";
+//        System.out.println(myString);
+
+//       char myString = 'J';
+//        System.out.println(myString);
+
+//        String myString = "3.14159";
+//        System.out.println(myString);
+//
+//        float myNumber = 1.23f;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        class
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+////        x = x + 5;
+//
+//        x +=5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+        x /= y;
+        System.out.println(x);
+        y -= x;
+        System.out.println(y);
+
+
+    }
 }
--- src/SyntaxTypesVariables.java
+++ src/SyntaxTypesVariables.java
@@ -1,6 +1,29 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Locale;
+
 public class SyntaxTypesVariables {
 
     public static void main(String[] args) {
 
+//        byte myFirstNumber = 127;
+//        byte mySecondNumber = 1;
+//        myFirstNumber += mySecondNumber;
+//        System.out.println(myFirstNumber);
+
+//        System.out.println((int)'H' + 'e' + 'l' + 'l' + 'o');....500
+
+//        String myName = "Jesse";
+//        System.out.println(myName.toLowerCase(Locale.ROOT));
+
+        byte temperature = 75;
+//        var isNiceDay = true;
+
+        if ((temperature > 60) && (temperature < 80 )){
+            System.out.println("What a nice day");
+        }else{
+            System.out.println("I don't like it");
+        }
     }
 }
+
