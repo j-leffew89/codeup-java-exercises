fbc70d856719c5faefce1312901b1b98f0d7371f
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-27 15:35:38 -0500

    exercise part 1 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,8 +2,32 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//
+//        }
 
+//        int num = 0;
+//        do {
+//            System.out.println(num);
+//            num +=2;
+//        }while(num <= 100);
 
+//        int num = 100;
+//
+//        do{
+//            System.out.println(num);
+//            num -=5;
+//        }while (num >= -10);
+//
+        long num = 2;
+
+        do {
+            System.out.println(num);
+            num *=num;
+        }while (num <= 1000000);
 
     }
 }
