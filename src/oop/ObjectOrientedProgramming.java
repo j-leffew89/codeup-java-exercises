dc92fb5cd1a998c582361ae2c9b0e46f4a6bddc7
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-30 12:32:19 -0500

    shhh

--- src/oop/ObjectOrientedProgramming.java
+++ src/oop/ObjectOrientedProgramming.java
@@ -2,8 +2,17 @@ package oop;
 
 public class ObjectOrientedProgramming {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // tell me about a person
+//        person person = new person("Jesse",32, "short", "USA");
+        person person = new person();
+        person.setName("Jesse");
+        System.out.println(person.getEntertainment("ride roller coasters"));
+
+        person person1 = new person();
+        person1.setName("Tj");
+        System.out.println(person1.getEntertainment("camping this weekend"));
+
     }
 
 }
---
+++ src/oop/person.java
@@ -0,0 +1,78 @@
+package oop;
+
+public class person {
+
+    // Class Members (all three are class members(field, constructor, properties))
+
+    // Field
+    private String name;
+    private int age;
+    private String height;
+    private String nationality;
+    private String purpose;
+    private static final boolean hasSkin = true;
+
+    // Constructor
+    public person(String name, int age,
+                  String height, String nationality) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.nationality = nationality;
+    }
+
+    public person() {
+
+    }
+
+    // Properties
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public static boolean isHasSkin() {
+        return hasSkin;
+    }
+
+    public String getEntertainment(String entertainment){
+        return this.name + " is going to go " + entertainment;
+    }
+
+    public String getPursose(){
+        return purpose;
+    }
+
+    public void setPursose(String purpose){
+        this.purpose = purpose;
+    }
+}
