125d2ccaf4d8941494946f6d2de24144fdf24380
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-30 20:20:24 -0500

    part 1

---
+++ src/Person.java
@@ -0,0 +1,54 @@
+public class Person {
+
+    public static void main(String[] args) {
+
+        Person person = new Person("Jesse ");
+
+//        person.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+    }
+
+
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public Person() {
+
+    }
+
+    public String getName(){
+//TODO: return the person's name
+        return this.name;
+    }
+
+    public void setName(String name){
+//TODO: change the name property to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+
+        System.out.println("Hello " + name);
+    }
+
+
+}
--- src/oop/ObjectOrientedProgramming.java
+++ src/oop/ObjectOrientedProgramming.java
@@ -4,14 +4,15 @@ public class ObjectOrientedProgramming {
 
     public static void main(String[] args) {
         // tell me about a person
-//        person person = new person("Jesse",32, "short", "USA");
-        person person = new person();
+        person person = new person("Jesse",32, "short", "USA");
+//        person person = new person();
         person.setName("Jesse");
         System.out.println(person.getEntertainment("ride roller coasters"));
 
         person person1 = new person();
         person1.setName("Tj");
         System.out.println(person1.getEntertainment("camping this weekend"));
+        System.out.println(person.getNationality());
 
     }
 
