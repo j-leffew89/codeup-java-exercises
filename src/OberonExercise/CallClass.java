f856e32032635e7ad6392d52a3f81e9ec6ed2ae3
Jesse Leffew <jesse.leffew89@gmail.com>
2021-07-01 11:59:49 -0500

    animals

--- src/OberonExercise/CallClass.java
+++ src/OberonExercise/CallClass.java
@@ -7,6 +7,8 @@ public class CallClass {
         Info info = new Info("Oberon", "Teach Coding", 23, 22);
 
         System.out.println(info.getCohortName());
+        System.out.println(info.getPurpose());
+        System.out.println(info.getNumberOfStudents());
 
 
 
---
+++ src/oop/animals/AquaMammal.java
@@ -0,0 +1,24 @@
+package oop.animals;
+
+public class AquaMammal extends MarineAnimal {
+
+   private int maxTimeSubmerged;
+
+   public AquaMammal(boolean canSwim, int length, int finCount,
+                     boolean isSaltwater, String name, int maxTimeSubmerged){
+       super(canSwim, length, finCount, isSaltwater, name);
+       this.maxTimeSubmerged = maxTimeSubmerged;
+   }
+
+    public AquaMammal(String name) {
+        super(name);
+    }
+
+    public int getMaxTimeSubmerged() {
+        return maxTimeSubmerged;
+    }
+
+    public void setMaxTimeSubmerged(int maxTimeSubmerged) {
+        this.maxTimeSubmerged = maxTimeSubmerged;
+    }
+}
