2b7c4323aa1cd5a68ab8af934ee10d79cdd39d94
Jesse Leffew <jesse.leffew89@gmail.com>
2021-06-30 22:30:25 -0500

    OOP exercises complete

--- src/shapes/CircleAPP.java
+++ src/shapes/CircleAPP.java
@@ -1,14 +1,18 @@
 package shapes;
 import util.Input;
+import java.text.DecimalFormat;
 
 public class CircleAPP {
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
+
     public static void main(String[] args){
         Input input = new Input();
-        System.out.println("What is the Radius of the circle");
+        System.out.println("What is the Radius of the circle: ");
         double radius = input.getDouble();
         System.out.println("The radius is " + radius);
         Circle circle = new Circle(radius);
-        System.out.println(circle.getArea());
-        System.out.println(circle.getCircumference());
+        System.out.println("The area is: " + df.format (circle.getArea()));
+        System.out.println("The circumference is: " + df.format (circle.getCircumference()));
     }
 }
