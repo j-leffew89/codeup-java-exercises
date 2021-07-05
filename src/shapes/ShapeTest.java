e66d536b740302cd44bae332dc06767d18f6dabd
Jesse Leffew <jesse.leffew89@gmail.com>
2021-07-04 19:22:55 -0500

    green square

--- src/shapes/ShapeTest.java
+++ src/shapes/ShapeTest.java
@@ -4,16 +4,17 @@ public class ShapeTest {
 
     public static void main(String[] args) {
         Square square1 = new Square(30);
-        System.out.println(square1.getPerimeter());
+        System.out.println("Square perimeter is: " + square1.getPerimeter());
 
         Rectangle box1 = new Rectangle(10,6);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
+        System.out.println("box1 perimeter is: " + box1.getPerimeter());
+        System.out.println("box1 area is: " + box1.getArea());
 
         Rectangle box2 = new Square(20);
-        System.out.println(box2.getPerimeter());
-        System.out.println(box2.getArea());
+        System.out.println("box2 perimeter is: " + box2.getPerimeter());
+        System.out.println("box2 area is: " + box2.getArea());
 
     }
+
 
 }
