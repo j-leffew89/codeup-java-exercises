8f7d770c95c5f0f841d359a75f65a08e31ee7091
Jesse Leffew <jesse.leffew89@gmail.com>
2021-07-09 10:14:35 -0500

    Vacation

---
+++ src/warmup/VacationSpots.java
@@ -0,0 +1,19 @@
+package warmup;
+
+import java.util.Scanner;
+
+public class VacationSpots {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // TODO: Create a method which will return a String[] containing the user's favorite vacation spots
+//  -> Each time the user inputs a new vacation spot, reset the array to be one element longer in length
+//  -> Then, add the old elements
+//  -> Finally, add the new element
+//  -> When the user decides to finish inputting spots, return the String[].
+//  -> sout out the returned value!
+
+
+    }
+}
