78467b233881ef03e480155bd5134fc11f7284a1
Jesse Leffew <jesse.leffew89@gmail.com>
2021-07-13 11:43:28 -0500

    colors

---
+++ src/Annotations/Colors.java
@@ -0,0 +1,25 @@
+package Annotations;
+
+class Red {
+    public void show(){
+        System.out.println("I am red");
+    }
+}
+
+class Orange extends Red {
+    @Override
+    public void show(){
+        System.out.println("I am orange");
+    }
+}
+
+public class Colors {
+    public static void main(String[] args) {
+        Red redobj = new Red();
+        Orange orangeobj = new Orange();
+
+        redobj.show();
+        orangeobj.show();
+    }
+
+}
